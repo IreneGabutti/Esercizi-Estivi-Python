8619fe830aaedfeb394af6bd7c464c3172d16219
IreneGabutti <irene.gabutti@itiscuneo.eu>
2020-06-16 20:43:27 +0200

    Add files via upload

---
+++ cap10_leListe/es_10.3/10.3.py
@@ -0,0 +1,12 @@
+#___Gabutti Irene___
+#Scrivete una funzione di nome mediani che prenda una lista e restituisca una
+#nuova lista che contenga tutti gli elementi, esclusi il primo e l’ultimo.
+
+def mediani(lista):
+    listaNuova = t[1:-1]
+    
+    print(lista)
+    print(listaNuova)
+
+t = [1,2,3,4,5,6]
+mediani(t)
